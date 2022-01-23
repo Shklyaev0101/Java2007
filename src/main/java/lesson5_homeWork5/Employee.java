5895b8905627156f63732f61ddf1725277d2826f
shklyaev0101 <ds.shklyaev@gmail.com>
2022-01-23 21:49:41 +0500

    HW_5_CW_HW(ex_3_from_5)

---
+++ src/main/java/lesson5_homeWork5/Employee.java
@@ -0,0 +1,26 @@
+package lesson5_homeWork5;
+
+public class Employee {
+
+    public String name;
+    public String post;
+    public String email;
+    public long telNumber;
+    public int age;
+    public int salary;
+
+
+    public Employee(String name, String post, String email, long telNumber, int age, int salary) {
+        this.name = name;
+        this.post = post;
+        this.email = email;
+        this.telNumber = telNumber;
+        this.age = age;
+        this.salary = salary;
+    }
+
+    public void info() {
+        System.out.println("name = " + this.name + "," + " post = " + this.post + "," + " email = " + this.email + "," + " telNumber = " + this.telNumber + "," + " age = " + this.age + "," + " salary = " + this.age + ";");
+    }
+
+}
---
+++ src/main/java/lesson5_homeWork5/homeWork5.java
@@ -0,0 +1,21 @@
+package lesson5_homeWork5;
+
+public class homeWork5 {
+
+    public static void main(String[] args) {
+        Employee employee = new Employee("Ivanov Ivan Ivanovich","lawyer", "didima@mailbox.com", 7999999999L, 33, 1000);
+
+        employee.info();
+        /*
+        employee.name = "Ivanov Ivan Ivanovich";
+        employee.post = "lawyer";
+        employee.email = "didima@mailbpox.com";
+        employee.telNumber = 7999999999L;
+        employee.age = 33;
+        employee.salary = 1000;
+        */
+
+
+        //System.out.println("name = " + employee.name + "," + " post = " + employee.post + "," + " email = " + employee.email + "," + " telNumber = " + employee.telNumber + "," + " age = " + employee.age + "," + " salary = " + employee.salary + ";");
+    }
+}
